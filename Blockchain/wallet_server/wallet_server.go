95c7d00719e65ecdcbd05de3ca9b78da897e3a76
monmon <syu24059@gmail.com>
2024-05-20 16:33:19 +0900

    Added Waller server

---
+++ Blockchain/wallet_server/wallet_server.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path"
+	"strconv"
+)
+
+const tempDir = "wallet_server/templates"
+
+type WalletServer struct {
+	port    uint16
+	gateway string
+}
+
+func NewWalletServer(port uint16, gateway string) *WalletServer {
+	return &WalletServer{port, gateway}
+}
+
+func (ws *WalletServer) Port() uint16 {
+	return ws.port
+}
+
+func (ws *WalletServer) Gateway() string {
+	return ws.gateway
+}
+
+func (ws *WalletServer) Index(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		t, err := template.ParseFiles(path.Join(tempDir, "index.html"))
+		if err != nil {
+			log.Printf("Error parsing template: %v", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		err = t.Execute(w, "")
+		if err != nil {
+			log.Printf("Error executing template: %v", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+	default:
+		log.Printf("Error: Invalid HTTP method")
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func (ws *WalletServer) Run() {
+	http.HandleFunc("/", ws.Index)
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(ws.Port())), nil))
+}
